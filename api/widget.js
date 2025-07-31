47d174146695e7f04e4019f57147fde4294ce043
mor1976 <57361913+mor1976@users.noreply.github.com>
2025-07-31 17:41:42 +0300

    Update widget.js

--- api/widget.js
+++ api/widget.js
@@ -5,10 +5,11 @@ export const config = {
 };
 
 module.exports = async (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-
   const body = req.body;
 
+  res.setHeader("Content-Type", "text/html");
+
+  // בדיקה בסיסית להצגת כל הנתונים שנשלחו מהטיקט
   res.send(`
     <h3>Payload שהתקבל מהטיקט:</h3>
     <pre>${JSON.stringify(body, null, 2)}</pre>
