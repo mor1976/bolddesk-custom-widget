f50b545b4bab90703fe84f9a36a499672df912a8
mor1976 <57361913+mor1976@users.noreply.github.com>
2025-07-31 17:22:00 +0300

    Update widget.js

--- api/widget.js
+++ api/widget.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
   const HUDU_API_KEY = "gAhGLUAioZb6Dbvk1FA6fgHD";
   const HUDU_BASE_URL = "https://get-mor.huducloud.com";
 
-  const email = req.body?.ticket?.requester?.email;
+  const email = req.query?.email;
   res.setHeader("Content-Type", "text/html");
 
   if (!email) {
